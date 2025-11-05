88c2058331d6c80ac5803910815cee8d29cc5a31
Aisiya Qutwatunnada <ycorp00@gmail.com>
2025-11-05 15:29:02 +0700

    refactor: clean up code structure and remove redundant sections

--- app/echo-server/main.go
+++ app/echo-server/main.go
@@ -1,3 +1,23 @@
+// @title Video Game Rental API
+// @version 1.0
+// @description Video Game Rental API adalah sistem backend untuk platform penyewaan game fisik
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Type "Bearer" followed by a space and JWT token.
+
 package main
 
 import (
@@ -14,6 +34,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/sirupsen/logrus"
 	echoSwagger "github.com/swaggo/echo-swagger"
+	_ "github.com/Yoochan45/go-game-rental-api/docs"
 )
 
 func main() {
